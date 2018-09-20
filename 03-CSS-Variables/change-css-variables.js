994a025317dc46e59b96987e67f4debd1f9ec2fe
Pasha Rybenko <rybenko.pasha@google.com>
2018-09-20 15:49:23 +0300

    fix eslint`s bugs

--- 03-CSS-Variables/change-css-variables.js
+++ 03-CSS-Variables/change-css-variables.js
@@ -1,13 +1,13 @@
-const inputs = document.querySelectorAll('.controls input')
-
-inputs.forEach(elem => {
-  elem.addEventListener('change', handleUpdate)
-})
-inputs.forEach(elem => {
-  elem.addEventListener('mousemove', handleUpdate)
-})
+let inputs = document.querySelectorAll('.controls input')
 
 function handleUpdate () {
   const suffix = this.dataset.sizing || ''
   document.querySelector('html').style.setProperty(`--${this.name}`, this.value + suffix)
 }
+
+inputs.forEach((elem) => {
+  elem.addEventListener('change', handleUpdate)
+})
+inputs.forEach((elem) => {
+  elem.addEventListener('mousemove', handleUpdate)
+})
